093d1fa90ea9f3751a79d37132ebd0e1a252c927
Julio Cesar Avila Saavedra <michavo2000@gmail.com>
2014-06-10 00:09:38 -0500

    modificaciones varias

--- js/js.js
+++ js/js.js
@@ -8,14 +8,28 @@
         return carouselStage.jcarousel('items').eq(itemNavigation.index());
     };
 
+	var img = 1;
+
+	setInterval(function() {
+
+		if (img == 3) {
+			$('.background.bg2').css("opacity", "0");
+			$('.background.bg' + img).css("opacity", "0");
+			img = 1;
+		} else {		
+			img++;
+			$('.background.bg' + img).css("opacity", "100");
+		}
+	}, 4000);
+
     $(function() {
         // Setup the carousels. Adjust the options for both carousels here.
-        var carouselStage      	= $('.carousel-stage').jcarousel();
-        var carouselNavigation 	= $('.carousel-navigation').jcarousel();
+        var carouselStage      	= $('.carousel-stage').jcarousel({auto:10});
+        var carouselNavigation 	= $('.carousel-navigation').jcarousel({auto:10});
         var arrow 				= $('.jcarousel-arrow');
 
-        arrow.on("click",function(){
-        	
+        arrow.on("click", function(){
+
         });
 
         // We loop through the items of the navigation carousel and set it up
@@ -87,3 +101,53 @@
             });
     });
 })(jQuery);
+
+// Cache selectors
+var lastId,
+    topMenu = $(".navbar"),
+    topMenuHeight = topMenu.outerHeight(),
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+  }, 300);
+  e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href=#"+id+"]").parent().addClass("active");
+   }                   
+});
+
+$(window).on("resize",function(){
+	$(".contacto").css("height", $(window).height() - 60);
+});
