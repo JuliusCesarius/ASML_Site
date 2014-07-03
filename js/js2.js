dbdcd1eb9c33927272079e8934e376f04c5ba50e
Julio Cesar Avila Saavedra <michavo2000@gmail.com>
2014-07-02 23:25:13 -0500

    Se corrigió estilos para móviles

--- js/js2.js
+++ js/js2.js
@@ -7,32 +7,37 @@
     var connector = function(itemNavigation, carouselStage) {
         return carouselStage.jcarousel('items').eq(itemNavigation.index());
     };
-    
-    if($(window).width()>600){
-      $("section").css("height", $(window).height() - 60);
-    }else{
-      $(".home, .quienes").css("min-height", $(window).height());
+
+    if($(window).height() > 600){
+  	 $("section").css("height", $(window).height() - 60);
     }
 
-    var img = 1;
+  	var img = 1;
 
-    setInterval(function() {
+  	// var timer = setInterval(function() {
 
-      if (img == 3) {
-        $('.background.bg2').css("opacity", "0");
-        $('.background.bg' + img).css("opacity", "0");
-        img = 1;
-      } else {    
-        img++;
-        $('.background.bg' + img).css("opacity", "100");
-      }
-    }, 4000);
+  	// 	if (img == 3) {
+  	// 		$('.background.bg2').css("opacity", "0");
+  	// 		$('.background.bg' + img).css("opacity", "0");
+  	// 		img = 1;
+  	// 	} else {		
+  	// 		img++;
+  	// 		$('.background.bg' + img).css("opacity", "100");
+  	// 	}
+  	// }, 4000);
+
+    $(window).on("scroll", function(){
+      $('.background').css("animation-play-state","paused")
+      //window.clearInterval(timer);
+      //element.style.webkitAnimationPlayState = "running";
+      
+    });
 
     $(function() {
         // Setup the carousels. Adjust the options for both carousels here.
-        var carouselStage       = $('.carousel-stage').jcarousel({auto:5});
-        var carouselNavigation  = $('.carousel-navigation').jcarousel({auto:5});
-        var arrow         = $('.jcarousel-arrow');
+        var carouselStage      	= $('.carousel-stage').jcarousel({auto:10});
+        var carouselNavigation 	= $('.carousel-navigation').jcarousel({auto:10});
+        var arrow 				= $('.jcarousel-arrow');
 
         arrow.on("click", function(){
 
@@ -106,57 +111,67 @@
                 target: '+=1'
             });
     });
+
+  // Cache selectors
+  var lastId,
+      topMenu = $(".navbar"),
+      topMenuHeight = null,
+      // All list items
+      menuItems = topMenu.find("a"),
+      // Anchors corresponding to menu items
+      scrollItems = menuItems.map(function(){
+        var item = $($(this).attr("href"));
+        if (item.length) { return item; }
+      });
+
+  $(".navbar-toggle").click(function(){
+    if (topMenuHeight == null){      
+        topMenuHeight = topMenu.outerHeight();
+    }
+  });
+
+  // Bind click handler to menu items
+  // so we can get a fancy scroll animation
+  menuItems.click(function(e){
+    if (topMenuHeight == null){      
+      topMenuHeight = topMenu.outerHeight();
+    }
+    var href = $(this).attr("href"),
+        offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+    $('html, body').stop().animate({ 
+        scrollTop: offsetTop
+    }, 300);
+    e.preventDefault();
+    $('.collapse').collapse('hide');
+  });
+
+  // Bind to scroll
+  $(window).scroll(function(){
+     // Get container scroll position
+     var fromTop = $(this).scrollTop()+topMenuHeight;
+     
+     // Get id of current scroll item
+     var cur = scrollItems.map(function(){
+       if ($(this).offset().top < fromTop)
+         return this;
+     });
+     // Get the id of the current element
+     cur = cur[cur.length-1];
+     var id = cur && cur.length ? cur[0].id : "";
+     
+     if (lastId !== id) {
+         lastId = id;
+         // Set/remove active class
+         menuItems
+           .parent().removeClass("active")
+           .end().filter("[href=#"+id+"]").parent().addClass("active");
+     }                   
+  });
+
+  $(window).on("resize",function(){
+    if($(window).height() > 600){
+      $("section").css("height", $(window).height() - 60);
+    }
+  });
+
 })(jQuery);
-
-// Cache selectors
-var lastId,
-    topMenu = $(".navbar"),
-    topMenuHeight = topMenu.outerHeight(),
-    // All list items
-    menuItems = topMenu.find("a"),
-    // Anchors corresponding to menu items
-    scrollItems = menuItems.map(function(){
-      var item = $($(this).attr("href"));
-      if (item.length) { return item; }
-    });
-
-// Bind click handler to menu items
-// so we can get a fancy scroll animation
-menuItems.click(function(e){
-  var href = $(this).attr("href"),
-      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
-  $('html, body').stop().animate({ 
-      scrollTop: offsetTop
-  }, 300);
-  e.preventDefault();
-  $('.collapse').collapse('hide')
-});
-
-// Bind to scroll
-$(window).scroll(function(){
-   // Get container scroll position
-   var fromTop = $(this).scrollTop()+topMenuHeight;
-   
-   // Get id of current scroll item
-   var cur = scrollItems.map(function(){
-     if ($(this).offset().top < fromTop)
-       return this;
-   });
-   // Get the id of the current element
-   cur = cur[cur.length-1];
-   var id = cur && cur.length ? cur[0].id : "";
-   
-   if (lastId !== id) {
-       lastId = id;
-       // Set/remove active class
-       menuItems
-         .parent().removeClass("active")
-         .end().filter("[href=#"+id+"]").parent().addClass("active");
-   }                   
-});
-
-$(window).on("resize",function(){
-  if($(window).width()>600){
-    $("section").css("height", $(window).height() - 60);
-  }
-});
